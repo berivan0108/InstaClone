c20189924df791609d7fb1b7ac21395a3c2a5049
berivan dağcı <100922483+berivan0108@users.noreply.github.com>
2026-07-06 21:33:08 +0300

    İlk commit: Projeye baslangic

---
+++ InstaClone/Core/TabBar/MainTabView.swift
@@ -0,0 +1,24 @@
+//
+//  MainTabView.swift
+//  InstaClone
+//
+//  Created by beri on 6.07.2026.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView{
+            Text("AAAA")
+            Text("AAAA")
+            Text("AAAA")
+            Text("AAAA")
+            Text("AAAA")
+        }
+    }
+}
+
+#Preview {
+    MainTabView()
+}
