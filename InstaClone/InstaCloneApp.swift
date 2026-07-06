7c747c50e56c13dc84333f5b9c2f70428d6c135f
berivan dağcı <100922483+berivan0108@users.noreply.github.com>
2026-07-06 21:19:16 +0300

    Initial Commit

---
+++ InstaClone/InstaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaCloneApp.swift
+//  InstaClone
+//
+//  Created by beri on 6.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct InstaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InstaCloneTests/InstaCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  InstaCloneTests.swift
+//  InstaCloneTests
+//
+//  Created by beri on 6.07.2026.
+//
+
+import Testing
+@testable import InstaClone
+
+struct InstaCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
